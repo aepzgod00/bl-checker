3fcef4f21c38969417afb40c3a610fef27e3548b
aepzgod00 <aepzgod00@gmail.com>
2026-06-26 00:52:43 +0700

    app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ else:
                     """
                     
                     response = client.models.generate_content(
-                        model='gemini-2.5-flash',
+                        model='gemini-3.5-flash',
                         contents=[bl_part, amend_part, system_instruction],
                         config=generation_config
                     )
